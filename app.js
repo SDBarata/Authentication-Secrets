c86189a45bc01dc80e59bc9ffc19394d85b72728
Barata <samuel.domingues_barata@siemens-energy.com>
2022-12-02 08:45:52 +0000

    Add harshing (Level 3)

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const encrypt = require("mongoose-encryption");
 const app = express();
+const md5 = require('md5');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+console.log(md5("qwerty"));
 main().catch(err => console.log(err));
 async function main() {
     await mongoose.connect("mongodb://localhost:27017/userDB")
@@ -20,8 +21,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -41,7 +40,7 @@ app.post("/register", (req, res) =>{
     console.log(req.body)
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if(!err){
@@ -54,7 +53,7 @@ app.post("/register", (req, res) =>{
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email: username}, function(err, foundUser){
         if(err){
             console.log(err);
